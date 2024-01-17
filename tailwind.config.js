96148c28952c70e31638830bf4672814aad490de
Juan Carlos Padillo <juancarlospadillo23@gmail.com>
2024-01-17 16:34:45 +0800

    starter template

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontSize:{
+        // headings
+        heading1: ["var(--heading-1)"],
+        heading2: ["var(--heading-2)"],
+        heading3: ["var(--heading-3)"],
+        heading4: ["var(--heading-4"],
+        heading5: ["var(--heading-5)"],
+
+        // subheadings
+        subheading1: ["var(--subheading-1)"],
+        subheading2: ["var(--subheading-2)"],
+
+        //body text
+        bodyFs:"var(--body-text)",
+      },
+      colors:{
+        primary: ["var(--black-clr)"],
+        lightPurple: ["var(--light-purple-clr)"],
+        light: ["var(--white-clr)"]
+      }
+    },
+    
+  },
+  plugins: [],
+}
+
